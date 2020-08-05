cda8a8e69fdda7358872bd43ea61e5dbaaf14d75
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-08-05 19:02:40 -0400

    feat: only move card when pointer is on card

--- public/index.js
+++ public/index.js
@@ -2,6 +2,9 @@ const canvas = document.getElementById('canvas')
 canvas.width = 600
 canvas.height = 600
 const ctx = canvas.getContext('2d')
+import deck from './cards'
+
+console.log(deck)
 
 const card = new Image()
 card.src = '/images/cards.png'
@@ -11,11 +14,29 @@ ctx.fillRect(0, 0, canvas.width, canvas.height)
 const mouse = {
 }
 
+const cardDeck = []
+
+const cards = {
+    height: 97,
+    width: 65
+}
+
+let currCard = deck[0]
+
 const frame = timeStamp => {
+    if (mouse.down && mouse.x >= currCard.xPos && mouse.x <= currCard.xPos + cards.width && mouse.y >= currCard.yPos && mouse.y <= currCard.yPos + cards.height) {
+        console.log(currCard.x + cards.width, currCard.y + cards.height)
+        console.log('hello testing')
+        currCard.xPos = mouse.x - 49
+        currCard.yPos = mouse.y - 33
+    } else {
+        currCard.xPos = 0
+        currCard.yPos = 0
+    }
     ctx.clearRect(0, 0, canvas.width, canvas.height)
-    ctx.fillStyle = 'blue'
+    ctx.fillStyle = 'red'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
-    ctx.drawImage(card, 195, 200, 65, 95, mouse.x - 32, mouse.y - 47, 65, 95)
+    ctx.drawImage(card, currCard.x, currCard.y, cards.width, cards.height, currCard.xPos, currCard.yPos + 5, cards.width, cards.height)
     window.requestAnimationFrame(frame)
 }
 
@@ -27,10 +48,18 @@ canvas.addEventListener('mousemove', evt => {
 
 canvas.addEventListener('mousedown', evt => {
     mouse.down = true
+    mouse.x = evt.clientX
+    mouse.y = evt.clientY
 })
 
-canvas.addEVentListener('mouseup', evt => {
+canvas.addEventListener('mouseup', evt => {
     mouse.down = false
+})
+
+window.addEventListener('keydown', evt => {
+    const cardIndex = deck.indexOf(currCard)
+    if (evt.key === 'ArrowUp') currCard = deck[cardIndex + 1] || deck[deck.length - 1]
+    if (evt.key === 'ArrowDown') currCard = deck[cardIndex - 1] || deck[0]
 })
 
 frame()
